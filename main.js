8ae9a48c91133710ef2545acee13286d8ab1b066
qoxogus <a01034367055@gmail.com>
2021-01-18 08:58:26 +0900

    [Study Hello World!]

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+/*
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -141,3 +149,4 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+*/
