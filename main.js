f5579bc0f7d68644906e9b0100a5fa50acb9d4d4
qoxogus <a01034367055@gmail.com>
2021-01-18 09:09:19 +0900

    [홈페이지 구현]

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var fs = require('fs');
+var template = require('./lib/template.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//route, routing
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(request, response) {
+  fs.readdir('./data', function(error, filelist) {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+      );
+      response.send(html);
+  });
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/page', function(req, res) {
+  return res.send('/page');
+})
+
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+})
 
 /*
 var http = require('http');
