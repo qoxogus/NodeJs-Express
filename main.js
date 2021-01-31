647f465b8d491ed1b981f7aeefd78b2ccaa1cc5b
qoxogus <a01034367055@gmail.com>
2021-01-31 11:46:25 +0900

    [use middleware]

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ var app = express()
 var fs = require('fs');
 var template = require('./lib/template.js');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //route, routing
 // app.get('/', (req, res) => res.send('Hello World!'))
@@ -65,19 +68,27 @@ app.get('/create', function(reqest, response){
 });
 
 app.post('/create_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data){
-     body = body + data;
-  });
-  request.on('end', function(){
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.writeHead(302, {Location: `/?id=${title}`});
-      response.end();
-    })
-  });
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.writeHead(302, {Location: `/?id=${title}`});
+    response.end();
+  })
+
+  // var body = '';
+  // request.on('data', function(data){
+  //    body = body + data;
+  // });
+  // request.on('end', function(){
+  //   var post = qs.parse(body);
+  //   var title = post.title;
+  //   var description = post.description;
+  //   fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+  //     response.writeHead(302, {Location: `/?id=${title}`});
+  //     response.end();
+  //   })
+  // });
 });
 
 app.get('/update/:pageId', function(request, response) {
@@ -107,36 +118,53 @@ app.get('/update/:pageId', function(request, response) {
 });
 
 app.post('/update_process', function(request, response) {
-  var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.redirect(`/?id=${title}`)
-            })
-          });
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/?id=${title}`)
+    })
+  });
+
+  // var body = '';
+  //     request.on('data', function(data){
+  //         body = body + data;
+  //     });
+  //     request.on('end', function(){
+  //         var post = qs.parse(body);
+  //         var id = post.id;
+  //         var title = post.title;
+  //         var description = post.description;
+  //         fs.rename(`data/${id}`, `data/${title}`, function(error){
+  //           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+  //             response.redirect(`/?id=${title}`)
+  //           })
+  //         });
+  //     });
 });
 
 app.post('/delete_process', function(request, response){
-  var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.redirect('/');
-          })
-      });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/');
+  });
+
+  // var body = '';
+  //     request.on('data', function(data){
+  //         body = body + data;
+  //     });
+  //     request.on('end', function(){
+  //         var post = qs.parse(body);
+  //         var id = post.id;
+  //         var filteredId = path.parse(id).base;
+  //         fs.unlink(`data/${filteredId}`, function(error){
+  //           response.redirect('/');
+  //         })
+  //     });
 });
 
 // app.listen(3000, () => console.log('Example app listening on port 3000!'))
