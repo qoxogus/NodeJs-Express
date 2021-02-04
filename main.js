f57fb5e06bcf8acd10bf70525ad8245b25f90214
qoxogus <a01034367055@gmail.com>
2021-02-05 03:57:05 +0900

    [add comment]

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ app.post('/delete_process', function(request, response){
 });
 
 // app.listen(3000, () => console.log('Example app listening on port 3000!'))
-app.listen(3000, function() {
+app.listen(3000, function() { //3000포트
   console.log('Example app listening on port 3000!')
 });
 
