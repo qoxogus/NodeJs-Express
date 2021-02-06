905ac33140028c2ba12f9e0cc7ea0dd221662d66
qoxogus <a01034367055@gmail.com>
2021-02-06 19:52:09 +0900

    [router 2]

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ var express = require('express')
 var app = express()
 var fs = require('fs');
 var template = require('./lib/template.js');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
 var compression = require('compression');
+var topicRouter = require('./routes/topic')
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,103 +32,7 @@ app.get('/', function(request, response) {
   response.send(html);
 });
 
-app.get('/topic/create', function(request, response){ // /topic/:pageId보다 먼저 실행함으로써 topic을 예약어로 쓰일수있음 (실행순서 중요해짐)
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
-
-app.post('/topic/create_process', function(request, response) {
-  // console.log(request.list);  //불러오지 못함 <undefined> (post라서)  13line 참조
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.redirect(`/topic/${title}`);
-  })
-});
-
-app.get('/topic/update/:pageId', function(request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <form action="/topic/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post('/topic/update_process', function(request, response) {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/topic/${title}`)
-    })
-  });
-});
-
-app.post('/topic/delete_process', function(request, response){
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    response.redirect('/');
-  });
-});
-
-app.get('/topic/:pageId', function(request, response, next) { // url = page/HTML      pageId = HTML
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    if(err) {
-      next(err); //아무 값도 주지않으면 다음 미들웨어 호출/ /route라면 미들웨어중에서 같은 인자가 아닌것 3개 넘기는것/ /그 외엔 err로 약속 (err를 던진다)
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-  });
-});
+app.use('/topic', topicRouter); // /topic 이므로 topic.js에서는 /topic을 빼야한다
 
 //미들웨어는 순차적으로 실행이 된다 그러므로 404에러처리 미들웨어는 가장 마지막에 위치한다.
 app.use(function(req, res, next) {
